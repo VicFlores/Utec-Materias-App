236d1448af1a2e68cc282f5e9617430472dc5ff1
Vic Flores <vicsito2014@gmail.com>
2021-12-31 15:43:33 -0600

    feat: Lesson page styles was created

---
+++ src/pages/Lessons.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from '../components/layout/Layout';
+import { LessonsForm } from '../components/lessonsForm/LessonsForm';
+
+export const Lessons = () => {
+  return (
+    <Layout>
+      <LessonsForm />
+    </Layout>
+  );
+};
