7ca657c8096efd9f4db42769f6c6a80de3bdce10
Vic Flores <vicsito2014@gmail.com>
2021-12-31 15:44:52 -0600

    feat: class detail page styles was created

---
+++ src/pages/ClassDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ClassDetailForm } from '../components/classDetailForm/ClassDetailForm';
+import { Layout } from '../components/layout/Layout';
+
+export const ClassDetails = () => {
+  return (
+    <Layout>
+      <ClassDetailForm />
+    </Layout>
+  );
+};
