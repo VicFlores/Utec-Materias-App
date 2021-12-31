92264f447503c097631cf513ffb66b92f68c8a85
Vic Flores <vicsito2014@gmail.com>
2021-12-31 15:39:53 -0600

    feat: Subject page style was created

---
+++ src/pages/Subjects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from '../components/layout/Layout';
+import { SubjectForm } from '../components/subjectForm/SubjectForm';
+
+export const Subjects = () => {
+  return (
+    <Layout>
+      <SubjectForm />
+    </Layout>
+  );
+};
