284583fef7fc5f3c618c80b59754317dc3f79667
Vic Flores <vicsito2014@gmail.com>
2021-12-31 15:31:36 -0600

    Home page style was created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from '../components/layout/Layout';
+import { Dashboard } from '../components/dashboard/Dashboard';
+
+export const Home = () => {
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
+};
