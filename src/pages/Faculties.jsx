1080913d9d4f5576c886469257372b0457564f91
Vic Flores <vicsito2014@gmail.com>
2021-12-31 15:41:01 -0600

    feat: faculties page styles was created

---
+++ src/pages/Faculties.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FacultyForm } from '../components/facultyForm/FacultyForm';
+import { Layout } from '../components/layout/Layout';
+
+export const Faculties = () => {
+  return (
+    <Layout>
+      <FacultyForm />
+    </Layout>
+  );
+};
