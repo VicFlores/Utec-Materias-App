7e2b1f081559b9ac8510b3b878fd3c191032da74
Vic Flores <vicsito2014@gmail.com>
2021-12-31 15:47:01 -0600

    feat: footer of app was created

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FooterContainer, FooterText } from './style';
+
+export const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterText>© Universidad Tecnológica de El Salvador</FooterText>
+      <FooterText>© Desarrollado por: Vic Ferman Flores Escobar</FooterText>
+    </FooterContainer>
+  );
+};
---
+++ src/components/footer/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  height: 50px;
+  background-color: #5d0a28;
+  color: #fcfcfc;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const FooterText = styled.h4`
+  font-size: 17px;
+  margin: 0;
+`;
