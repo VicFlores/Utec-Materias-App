c3b06962a593d58eb45430096c459b9fdb1a1da8
Vic Flores <vicsito2014@gmail.com>
2021-12-31 15:37:49 -0600

    refactor: unused import was removed

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Layout } from '../components/layout/Layout';
-import { UserForm } from '../components/userForm/UserForm';
 import { UserTable } from '../components/userTable/UserTable';
 
 export const Users = () => {
