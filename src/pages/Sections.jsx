b46e4e823c5f060c3d2f8099c67f18295bfcf2ed
Vic Flores <vicsito2014@gmail.com>
2021-12-31 15:42:10 -0600

    feat: Section page styles was created

---
+++ src/pages/Sections.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from '../components/layout/Layout';
+import { SectionForm } from '../components/sectionForm/SectionForm';
+
+export const Sections = () => {
+  return (
+    <Layout>
+      <SectionForm />
+    </Layout>
+  );
+};
