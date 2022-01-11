84ac1d2a37d9edc93b9eb055dbf0ce78b5dd8542
Vic Flores <vicsito2014@gmail.com>
2022-01-10 20:02:58 -0600

    feat: form for register faculty was created

--- src/components/facultyForm/FacultyForm.jsx
+++ src/components/facultyForm/FacultyForm.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import jwt_decode from 'jwt-decode';
+import { AuthContext } from '../../context/AuthContext';
 import {
   ButtonFormContainer,
   Col,
@@ -12,25 +15,57 @@ import {
   Card,
   Title,
 } from '../../pages/GlobalStyle';
+import { newFaculty } from '../../helpers/newFaculty';
+import { Layout } from '../layout/Layout';
+import { useNavigate } from '@reach/router';
 
 export const FacultyForm = () => {
+  const [message, setMessage] = useState('');
+  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const decoded = jwt_decode(auth);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const result = await newFaculty(auth, decoded, data);
+
+    if (result.message) {
+      return setMessage(result.message);
+    }
+
+    navigate('/faculties');
+  };
+
   return (
-    <>
+    <Layout>
       <Title>Faculty Register</Title>
       <Container>
-        <FormContainer>
-          <Card>
+        <FormContainer onSubmit={handleSubmit(onSubmit)}>
+          <Card className="animate__animated animate__fadeIn">
             <Row>
               <Col md={12}>
                 <Item>
                   <Label>Name of faculty:</Label>
-                  <Input />
+                  <Input
+                    type="text"
+                    {...register('name', { required: true })}
+                  />
+                  {errors.name && <span>This field is required</span>}
                 </Item>
               </Col>
               <Col md={12}>
                 <Item>
                   <Label>School of subject:</Label>
-                  <Input />
+                  <Input
+                    type="text"
+                    {...register('school', { required: true })}
+                  />
+                  {errors.school && <span>This field is required</span>}
                 </Item>
               </Col>
               <Col md={12}>
@@ -42,6 +77,6 @@ export const FacultyForm = () => {
           </Card>
         </FormContainer>
       </Container>
-    </>
+    </Layout>
   );
 };
