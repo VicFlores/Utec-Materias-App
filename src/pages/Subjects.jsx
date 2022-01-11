6b078cb88930cb3d6a38b38d5233247b8593273f
Vic Flores <vicsito2014@gmail.com>
2022-01-10 20:14:08 -0600

    feat: subject page was created

--- src/pages/Subjects.jsx
+++ src/pages/Subjects.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Layout } from '../components/layout/Layout';
-import { SubjectForm } from '../components/subjectForm/SubjectForm';
+import { SubjectTable } from '../components/subjectTable/SubjectTable';
 
 export const Subjects = () => {
   return (
     <Layout>
-      <SubjectForm />
+      <SubjectTable />
     </Layout>
   );
 };
