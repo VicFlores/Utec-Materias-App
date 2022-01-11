45e4ed14e53cfd8d5bcdfce6761230f8d9029956
Vic Flores <vicsito2014@gmail.com>
2022-01-10 20:05:48 -0600

    feat: not found was added

---
+++ src/components/notFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Img, ImgContainer } from './style';
+
+export const NotFound = () => {
+  return (
+    <ImgContainer>
+      <Img src="https://res.cloudinary.com/vicflores11/image/upload/v1641072384/undraw_page_not_found_re_e9o6_ez5aly.svg" />
+    </ImgContainer>
+  );
+};
---
+++ src/components/notFound/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ImgContainer = styled.figure`
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Img = styled.img`
+  margin: 0;
+`;
