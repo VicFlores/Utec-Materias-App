cedef3312f50ac6757513e014c33fdf92af05c9b
Vic Flores <vicsito2014@gmail.com>
2022-01-10 20:08:52 -0600

    feat: form register user was added

--- src/components/userForm/UserForm.jsx
+++ src/components/userForm/UserForm.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from '@reach/router';
 import { Layout } from '../layout/Layout';
 import {
   ButtonFormContainer,
@@ -13,37 +16,74 @@ import {
   Card,
   Title,
 } from '../../pages/GlobalStyle';
+import { AuthContext } from '../../context/AuthContext';
+import { newUser } from '../../helpers/newUser';
 
 export const UserForm = () => {
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+  const { auth } = useContext(AuthContext);
+  const decoded = jwt_decode(auth);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const result = await newUser(auth, decoded, data);
+    if (result.message) {
+      return setMessage(result.message);
+    }
+    navigate('/users');
+  };
+
   return (
     <Layout>
       <Title>User Register</Title>
       <Container>
-        <FormContainer>
-          <Card>
+        <FormContainer onSubmit={handleSubmit(onSubmit)}>
+          <Card className="animate__animated animate__fadeIn">
             <Row>
               <Col md={12}>
                 <Item>
                   <Label>Names of teacher:</Label>
-                  <Input />
+                  <Input
+                    type="text"
+                    {...register('firstname', { required: true })}
+                  />
+                  {errors.firstname && <span>This field is required</span>}
                 </Item>
               </Col>
               <Col md={12}>
                 <Item>
                   <Label>Lastname of teacher:</Label>
-                  <Input />
+                  <Input
+                    type="text"
+                    {...register('lastname', { required: true })}
+                  />
+                  {errors.lastname && <span>This field is required</span>}
                 </Item>
               </Col>
               <Col md={12}>
                 <Item>
                   <Label>Institutional mail</Label>
-                  <Input />
+                  <Input
+                    type="email"
+                    {...register('email', { required: true })}
+                  />
+                  {errors.email && <span>This field is required</span>}
                 </Item>
               </Col>
               <Col md={12}>
                 <Item>
                   <Label>Password</Label>
-                  <Input />
+                  <Input
+                    type="password"
+                    {...register('passwd', { required: true })}
+                  />
+                  {errors.passwd && <span>This field is required</span>}
                 </Item>
               </Col>
               <Col md={12}>
